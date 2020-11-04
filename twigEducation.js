92665b8a1a44d49cb62b0af4d70123069526b040
fraser steel <frasersteel@btinternet.com>
2020-11-04 23:16:13 +0000

    removed unnecessary for each

--- twigEducation.js
+++ twigEducation.js
@@ -1,9 +1,9 @@
 /*
-Given an array of length >= 0, and a positive integer N, return the contents of the array divided into N
-equally sized arrays.
+    Given an array of length >= 0, and a positive integer N, return the contents of the array divided into N
+    equally sized arrays.
 
-Where the size of the original array cannot be divided equally by N, the final part should have a length equal
-to the remainder.
+    Where the size of the original array cannot be divided equally by N, the final part should have a length equal
+    to the remainder.
  */
 
 
@@ -17,16 +17,15 @@ const sortArray = (array, arrSize) => {
   for(var i = 0; i<arrSize; i++){
     finalArray.push([]);
   }
-  finalArray.forEach((item, i) => {
-    for(var x = 0; x<normalArraySize; x++){
+  for(var i = 0; i < arrSize; i++){
+    for(var x = 0; x<normalArraySize; x++) {
       if(array[counter] === undefined){
         break;
       }
       finalArray[i].push(array[counter]);
       counter++;
     }
-  });
-
+  }
   return finalArray;
 }
 
